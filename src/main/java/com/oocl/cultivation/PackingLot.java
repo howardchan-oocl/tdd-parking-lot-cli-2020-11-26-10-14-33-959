d4872a9c66c627d183f60e8243b30d515ee45897
chanho <howard.chan@oocl.com>
2020-11-27 15:20:10 +0800

    [Parking][Howard Chan]feat:implement the function of should_return_ticket_when_pack_a_car_given_parkingLot_with_capacity

--- src/main/java/com/oocl/cultivation/PackingLot.java
+++ src/main/java/com/oocl/cultivation/PackingLot.java
@@ -1,13 +1,23 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PackingLot {
     private final int capacity;
+    private Map<Ticket, Car> ticketCarMap;
 
     public PackingLot(int capacity) {
         this.capacity = capacity;
+        ticketCarMap = new HashMap<>();
     }
 
     public Ticket park(Car car) {
-        return null;
+        if (capacity - ticketCarMap.size() <= 0) {
+            return null;
+        }
+        Ticket ticket = new Ticket();
+        ticketCarMap.put(ticket,car);
+        return ticket;
     }
 }

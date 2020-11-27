35a620c3b55d13a90fa7f2ce23bec1f510ca3644
chanho <howard.chan@oocl.com>
2020-11-27 15:13:30 +0800

    [Parking][Howard Chan]feat:implement the test of should_return_ticket_when_pack_a_car_given_parkingLot_with_capacity

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -2,9 +2,20 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
+    void should_return_ticket_when_pack_a_car_given_parkingLot_with_capacity() {
+        //given
+        PackingLot packingLot = new PackingLot(1);
+        Car car = new Car();
 
+        //when
+        Ticket ticket = packingLot.park(car);
+
+        //then
+        assertNotNull(ticket);
     }
 }

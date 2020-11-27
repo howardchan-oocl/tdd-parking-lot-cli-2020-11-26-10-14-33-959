7bf619025859845a30c09f1abe27c1319ea1b2b8
chanho <howard.chan@oocl.com>
2020-11-27 15:52:32 +0800

    [Parking][Howard Chan]feat:implement the packingBoy test of should_return_ticket_when_call_pack_of_parking_lot_given_parkingLot_with_capacity

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,4 +1,13 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
+    private final PackingLot packingLot;
+
+    public ParkingBoy(PackingLot packingLot) {
+        this.packingLot = packingLot;
+    }
+
+    public Ticket park(Car car) {
+        return null;
+    }
 }
--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -5,5 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingBoyTest {
+    @Test
+    void should_return_ticket_when_call_pack_of_parking_lot_given_parkingLot_with_capacity() {
+        //given
+        PackingLot packingLot = new PackingLot(1);
+        ParkingBoy packingBoy = new ParkingBoy(packingLot);
+        Car car = new Car();
 
+        //when
+        Ticket ticket = packingBoy.park(car);
+
+        //then
+        assertNotNull(ticket);
+    }
 }

a9402150cc4afa09a9845a7fa799cde3023ee292
chanho <howard.chan@oocl.com>
2020-11-29 13:44:37 +0800

    [Parking story6][Howard Chan]feat:implement the class ParkingLotServiceManager extended from NotThatCleverParkingBoy and test all the functions inherited from NotThatCleverParkingBoy

---
+++ src/main/java/com/oocl/cultivation/ParkingLotServiceManager.java
@@ -0,0 +1,9 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class ParkingLotServiceManager extends NotThatCleverParkingBoy{
+    public ParkingLotServiceManager(List<ParkingLot> parkingLots) {
+        super(parkingLots);
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/ParkingLotServiceManagerTest.java
@@ -0,0 +1,83 @@
+package com.oocl.cultivation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParkingLotServiceManagerTest {
+    @Test
+    void should_return_ticket_that_park_a_car_to_second_parking_lot_when_call_park_car_with_full_first_parking_lot() throws NotEnoughPositionException {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLot1 = new ParkingLot(0);
+        ParkingLot parkingLot2 = new ParkingLot(1);
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        ParkingLotServiceManager parkingLotServiceManager = new ParkingLotServiceManager(parkingLots);
+        Car car = new Car();
+
+        //when
+        Ticket ticket = parkingLotServiceManager.park(car);
+
+        //then
+        assertNotNull(ticket);
+    }
+
+    @Test
+    void should_throw_NotEnoughPositionException_when_call_park_car_with_no_parking_lot_with_enough_position() {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLot1 = new ParkingLot(0);
+        ParkingLot parkingLot2 = new ParkingLot(0);
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        ParkingLotServiceManager parkingLotServiceManager = new ParkingLotServiceManager(parkingLots);
+        Car car = new Car();
+
+        //when
+        NotEnoughPositionException notEnoughPositionException = assertThrows(NotEnoughPositionException.class, () -> parkingLotServiceManager.park(car));
+
+        //then
+        assertEquals("Not Enough Position", notEnoughPositionException.getMessage());
+    }
+
+    @Test
+    void should_return_car_that_fetch_a_car_from_second_parking_lot_when_call_fetch_car_that_parked_in_second_parking_Lot() throws NotEnoughPositionException, UnrecognizedParkingTicketException {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLot1 = new ParkingLot(0);
+        ParkingLot parkingLot2 = new ParkingLot(1);
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        ParkingLotServiceManager parkingLotServiceManager = new ParkingLotServiceManager(parkingLots);
+        Car actual = new Car();
+        Ticket ticket = parkingLotServiceManager.park(actual);
+
+
+        //when
+        Car expected = parkingLotServiceManager.fetch(ticket);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_throw_UnrecognizedParkingTicketException_that_fetch_a_car_by_an_invalid_ticket() {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLot1 = new ParkingLot(0);
+        ParkingLot parkingLot2 = new ParkingLot(1);
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        ParkingLotServiceManager parkingLotServiceManager = new ParkingLotServiceManager(parkingLots);
+
+        //when
+        UnrecognizedParkingTicketException unrecognizedParkingTicketException = assertThrows(UnrecognizedParkingTicketException.class, () -> parkingLotServiceManager.fetch(new Ticket()));
+
+        //then
+        assertEquals("Unrecognized Parking Ticket", unrecognizedParkingTicketException.getMessage());
+    }
+}

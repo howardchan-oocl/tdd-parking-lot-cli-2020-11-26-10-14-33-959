beaa95658efc4a3dc8e37c924c331a402679a851
chanho <howard.chan@oocl.com>
2020-11-29 12:36:58 +0800

    [Parking story3][Howard Chan]feat:implement the function of should_throw_NotEnoughPositionException_when_call_park_car_with_no_parking_lot_with_enough_position

--- src/main/java/com/oocl/cultivation/NotThatCleverParkingBoy.java
+++ src/main/java/com/oocl/cultivation/NotThatCleverParkingBoy.java
@@ -18,6 +18,10 @@ public class NotThatCleverParkingBoy {
             }
         }
 
+        if (ticket == null) {
+            throw new NotEnoughPositionException();
+        }
+
         return ticket;
     }
 

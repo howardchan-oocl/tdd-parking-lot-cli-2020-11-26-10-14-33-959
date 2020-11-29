c57e7b9f23fbd2e3c4532f8d8e42a49c491be45b
chanho <howard.chan@oocl.com>
2020-11-29 13:07:14 +0800

    [Parking story4][Howard Chan]feat:implement the test case of should_return_ticket_that_park_a_car_to_the_parking_lot_with_more_empty_postion_when_call_park_car

---
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -0,0 +1,17 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class SmartParkingBoy {
+    private final List<ParkingLot> parkingLots;
+
+    public SmartParkingBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
+    }
+
+    public Ticket park(Car car) throws NotEnoughPositionException {
+        Ticket ticket = null;
+
+        return ticket;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -0,0 +1,28 @@
+package com.oocl.cultivation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SmartParkingBoyTest {
+    @Test
+    void should_return_ticket_that_park_a_car_to_the_parking_lot_with_more_empty_postion_when_call_park_car() throws NotEnoughPositionException {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car = new Car();
+
+        //when
+        Ticket ticket = smartParkingBoy.park(car);
+
+        //then
+        assertTrue(parkingLot2.IsTicketValid(ticket));
+    }
+}
